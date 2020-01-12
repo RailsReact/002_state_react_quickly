b807eec4e945aa97b6e2415534affe78f9f06384
excavator <str_token@mail.ru>
2020-01-12 21:25:44 +0300

    add state

---
+++ clock/js/clock.js
@@ -0,0 +1,15 @@
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: new Date().toLocaleString()
+    };
+  }
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      this.state.currentTime
+    );
+  }
+}
---
+++ clock/jsx/clock.jsx
@@ -0,0 +1,11 @@
+class Clock extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentTime: (new Date()).toLocaleString()
+    }
+  }
+  render() {
+    return <div>{ this.state.currentTime }</div>
+  }
+}
